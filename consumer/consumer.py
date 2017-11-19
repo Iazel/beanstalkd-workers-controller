9628cf30fd9d3620d3e9569365190e4f700df84e
Iazel <pelligra.s@gmail.com>
2017-11-19 22:54:44 +0100

    downscale and more logs

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -3,9 +3,11 @@ import time
 import random
 import beanstalkc
 
+tube = os.environ['QUEUE']
 bs = beanstalkc.Connection(host='beanstalkd', port=11300)
-bs.use(os.environ['QUEUE'])
+bs.use(tube)
 
 while(True):
     bs.reserve().delete()
+    print "Remove job from " + tube
     time.sleep(random.randint(1, 5))
--- producer/producer.py
+++ producer/producer.py
@@ -4,7 +4,9 @@ import time
 
 bs = beanstalkc.Connection(host='beanstalkd', port=11300)
 while(True):
-    for i in range(10):
-        bs.use("tube-" + str(random.randint(0, 10)))
+    for i in range(3):
+        tube = "tube-" + str(random.randint(0, 5))
+        bs.use(tube)
         bs.put("Hello World")
+        print "Pushed a job to " + tube
     time.sleep(5)
